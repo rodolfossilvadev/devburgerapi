01b5196ecd8c56781c78bff635695e1ca68271a4
rodolfossilvadev <rodolfodev30@gmail.com>
2024-12-02 20:21:17 -0300

    tudo-funcionando-no-render

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 module.exports = {
     dialect: 'postgres',
-    url: 'postgresql://postgres:EEgPlibvPqduaVnHEFrrKszOetYCeRCa@junction.proxy.rlwy.net:13636/railway',
+    url: 'postgresql://postgres_devburger_user:afGvzL6N5fNSPHU6a13t0TZ5ZCLpl66A@dpg-ct734jaj1k6c73b2p6e0-a.oregon-postgres.render.com/postgres_devburger',
     define: {
         timestamps: true,
         underscored: true,
         underscoredAll: true,
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
     },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,9 @@
 import Sequelize from "sequelize";
 import mongoose from "mongoose";
 
-
 import User from "../app/models/user.js";
 import Products from "../app/models/products.js";
 import Category from "../app/models/category.js";
-
-
 
 const models = [User, Products, Category];
 
@@ -15,17 +12,24 @@ class Database {
         this.init();
         this.mongo();
     }
+
     init() {
-        this.connection = new Sequelize('postgresql://postgres:EEgPlibvPqduaVnHEFrrKszOetYCeRCa@junction.proxy.rlwy.net:13636/railway');
+        this.connection = new Sequelize('postgresql://postgres_devburger_user:afGvzL6N5fNSPHU6a13t0TZ5ZCLpl66A@dpg-ct734jaj1k6c73b2p6e0-a.oregon-postgres.render.com/postgres_devburger', {
+            dialect: 'postgres',
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                },
+            },
+        });
+
         models.map(model => model.init(this.connection))
-            .map(
-                model => model.associate && model.associate(this.connection.models),
-            );
+            .map(model => model.associate && model.associate(this.connection.models));
     }
+
     mongo() {
-        this.mongoConnection = mongoose.connect(
-            process.env.MONGO_URL
-        );
+        this.mongoConnection = mongoose.connect(process.env.MONGO_URL);
     }
 }
 
