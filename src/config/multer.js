f93dc5c415a146c6fcf137fecd8fd23a56b5dc35
rodolfossilvadev <rodolfodev30@gmail.com>
2024-12-02 23:05:55 -0300

    corrigindo erro de imagem

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,13 +1,17 @@
-import multer from "multer";
-import { v4 } from "uuid";
-import { extname, resolve } from "node:path"
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import multerConfig from './multerConfig';
 
-export default {
-    storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', 'Uploads'),
-        filename: (request, file, callback) => {
-            return callback(null, v4() + extname(file.originalname));
-        }
-    }),
-}
+const app = express();
+const upload = multer(multerConfig);
 
+app.post('/upload', upload.single('file'), (req, res) => {
+    return res.json({ file: req.file });
+});
+
+app.use('/product-file', express.static(path.resolve(__dirname, '..', '..', 'Uploads')));
+
+app.listen(3001, () => {
+    console.log("Servidor rodando na porta 3001...");
+});
