b75ac9014df95a59bc88e9beadf52f2bf1543bed
rodolfossilvadev <rodolfodev30@gmail.com>
2024-12-02 21:49:40 -0300

    corrigindo cors

--- src/app.js
+++ src/app.js
@@ -7,14 +7,21 @@ import './database';
 
 class App {
     constructor() {
-        this.app = express()
+        this.app = express();
 
-        this.app.use(cors());
+        this.app.use(cors({
+            origin: 'https://joyful-cat-76ad2f.netlify.app',
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        }));
+
         this.middlewares();
         this.routes();
     }
+
     middlewares() {
         this.app.use(express.json());
+
         this.app.use(
             '/product-file',
             express.static(resolve(__dirname, '..', 'uploads'))
