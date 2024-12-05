3ea1b78ea00416133e90f62d5bf45dfd0e2217c5
rodolfossilvadev <rodolfodev30@gmail.com>
2024-12-04 22:50:10 -0300

    correcao imagens

--- src/app.js
+++ src/app.js
@@ -7,28 +7,28 @@ import './database';
 
 const corsOptions = {
     origin: 'https://joyful-cat-76ad2f.netlify.app',
-    credentials: true
-}
+    credentials: true,
+};
 
 class App {
-
-
     constructor() {
-        this.app = express()
+        this.app = express();
 
         this.app.use(cors(corsOptions));
         this.middlewares();
         this.routes();
     }
+
     middlewares() {
         this.app.use(express.json());
+
         this.app.use(
             '/product-file',
-            express.static(resolve(__dirname, '..', 'uploads'))
+            express.static(resolve(__dirname, '..', 'uploads', 'products'))
         );
         this.app.use(
             '/category-file',
-            express.static(resolve(__dirname, '..', 'uploads'))
+            express.static(resolve(__dirname, '..', 'uploads', 'categories'))
         );
     }
 
